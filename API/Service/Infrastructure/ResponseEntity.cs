14352dcfc2edf7b2afb14fe87a753057cad207c7
Ramesh <50228435+rameshbalakrishnan@users.noreply.github.com>
2019-06-10 19:15:53 +0530

    Add files via upload

---
+++ API/Service/Infrastructure/ResponseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Collections.Generic;
+namespace API.Service.Infrastructure
+{
+    public class ResponseEntity<T>
+    {
+        public int Responsecode { get; set; }
+        public string ResponseMessage { get; set; }
+        public T Entity { get; set; }
+        public IEnumerable<T> EnityList { get; set; }
+    }
+
+}
