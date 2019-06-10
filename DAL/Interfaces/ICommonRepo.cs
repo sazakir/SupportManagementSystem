a72394fa2a9ca1f6ebdd54e158add12a60fbf16d
Ramesh <50228435+rameshbalakrishnan@users.noreply.github.com>
2019-06-10 19:26:44 +0530

    Add files via upload

---
+++ DAL/Interfaces/ICommonRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DAL.Model;
+using System.Linq;
+using System.Linq.Expressions;
+using MongoDB.Driver.Linq;
+using MongoDB.Driver;
+namespace DAL.Interfaces
+{
+    public interface ICommonRepo<T> where T : class
+    {
+        IEnumerable<T> FindAll();
+        IEnumerable<T> FindByCondition(Expression<Func<T, bool>> expression);
+        void Create(T entity);
+        void Update(Expression<Func<T, bool>> expression, UpdateDefinition<T> entity);
+        void Delete(Expression<Func<T, bool>> expression);
+        void BuklkInsert(IEnumerable<T> items);
+    }
+}
