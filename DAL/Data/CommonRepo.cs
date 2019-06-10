861ac8a358c33269a414bc2ff3fc6165eef7ce72
Ramesh <50228435+rameshbalakrishnan@users.noreply.github.com>
2019-06-10 19:27:15 +0530

    Add files via upload

---
+++ DAL/Data/CommonRepo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using DAL.Model;
+using System.Threading.Tasks;
+using DAL.Interfaces;
+using System.Collections.Generic;
+using Microsoft.Extensions.Options;
+using System.Linq;
+using System.Linq.Expressions;
+using MongoDB.Driver.Linq;
+
+namespace DAL.Data
+{
+    public class CommonRepo<T> : ICommonRepo<T> where T : class
+    {
+
+        private readonly DBcontext<T> _context = null;
+
+        public CommonRepo(IOptions<Settings> settings)
+        {
+            _context = new DBcontext<T>(settings);
+        }
+
+
+        public IEnumerable<T> FindAll()
+        {
+            return _context.CommonEntity.Find(_ => true).ToList();
+        }
+
+        public IEnumerable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return this._context.CommonEntity.Find(expression).ToList();
+        }
+
+        public void Create(T entity)
+        {
+            this._context.CommonEntity.InsertOne(entity);
+        }
+
+        public void Update(Expression<Func<T, bool>> expression, UpdateDefinition<T> entity)
+        {
+            this._context.CommonEntity.UpdateOne(expression, entity);
+        }
+
+        public void Delete(Expression<Func<T, bool>> expression)
+        {
+            this._context.CommonEntity.DeleteOne(expression);
+        }
+        public void BuklkInsert(IEnumerable<T> items)
+        {
+            this._context.CommonEntity.InsertMany(items);
+        }
+
+    }
+}
---
+++ DAL/Data/DBcontext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using DAL.Model;
+using System.Collections.Generic;
+namespace DAL.Data
+{
+    public class DBcontext<T> where T : class
+    {
+        private readonly IMongoDatabase _database = null;
+
+        public DBcontext(IOptions<Settings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            if (client != null)
+                _database = client.GetDatabase(settings.Value.Database);
+        }
+
+        public IMongoCollection<T> CommonEntity
+        {
+            get
+            {
+                return _database.GetCollection<T>(typeof(T).Name);
+            }
+        }
+    }
+}
